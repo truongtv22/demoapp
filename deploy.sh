1456a1a48922ee843a751751a00be27d3056ea55
Tạ Trường <truongtv22@gmail.com>
2022-06-08 21:27:56 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -70,13 +70,13 @@ if [ $DEPLOY_TYPE == "hard" ]; then
     echo -e "${GREEN}- - - - -"
     echo -e "Fastlane 🍎  iOS $APP_ENV"
     echo -e "- - - - -${NO_COLOR}"
-    bundle exec fastlane ios deploy --env=$APP_ENV
+    bundle exec fastlane ios deploy --env $APP_ENV
   fi
   if [[ $APP_OS != "ios" ]]; then
     echo -e "${YELLOW}- - - - -"
     echo "Fastlane 🤖  Android $APP_ENV"
     echo -e "- - - - -${NO_COLOR}"
-    bundle exec fastlane android deploy --env=$APP_ENV
+    bundle exec fastlane android deploy --env $APP_ENV
   fi
 fi
 
@@ -89,13 +89,13 @@ if [ $DEPLOY_TYPE == "soft" ]; then
     echo -e "${GREEN}- - - - -"
     echo -e "Codepush 🍎  iOS ${APP_ENV}"
     echo -e "- - - - -${NO_COLOR}"
-    bundle exec fastlane ios deploy codepush: --env=$APP_ENV
+    bundle exec fastlane ios deploy codepush: --env $APP_ENV
   fi
   if [[ $APP_OS != "ios" ]]; then
     echo -e "${YELLOW}- - - - -"
     echo -e "Codepush 🤖  Android ${APP_ENV}"
     echo -e "- - - - -${NO_COLOR}"
-    bundle exec fastlane android deploy codepush: --env=$APP_ENV
+    bundle exec fastlane android deploy codepush: --env $APP_ENV
   fi
 fi
 
