edf0135c492994a7e2f12abbd9e7dc10452f7b0d
Tạ Trường <truongtv22@gmail.com>
2022-06-08 17:25:38 +0700

    Wrap with code push

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { registerRootComponent } from 'expo';
+import codePush from 'react-native-code-push';
 
 import App from './App';
+import { ENV } from './src/environment';
+
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_SUSPEND,
+  minimumBackgroundDuration: ENV === 'production' ? 120 : 0,
+};
+const CodePushedApp = codePush(codePushOptions)(App);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(CodePushedApp);
