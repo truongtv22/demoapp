b4a01ed6dbd152e5a71ff06daeb81667baa56fd6
Tạ Trường <truongtv22@gmail.com>
2022-06-08 23:01:48 +0700

    Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Update App.js</Text>
+      <Text>Edit App.js</Text>
       <StatusBar style="auto" />
     </View>
   );
